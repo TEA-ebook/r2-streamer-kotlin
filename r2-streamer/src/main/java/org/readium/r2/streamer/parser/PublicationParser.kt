510a99216c290993f0277c5e1dc0359cd9279c5a
Johan Poirier <johan.poirier@gmail.com>
2018-07-25 16:08:26 +0200

    Remove line comment on bug fix

--- r2-streamer/src/main/java/org/readium/r2/streamer/parser/PublicationParser.kt
+++ r2-streamer/src/main/java/org/readium/r2/streamer/parser/PublicationParser.kt
@@ -39,5 +39,5 @@ fun normalize(base: String, href: String?): String {
     for (component in normalizedComponents) {
         normalizedString += "/$component"
     }
-    return normalizedString//.substring(1)
+    return normalizedString.substring(1)
 }
